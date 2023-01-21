633ccdcc6f971a8aca870007df2eee88bf583e4e
SHAFI <118980998+mmshafi22@users.noreply.github.com>
2023-01-21 23:11:24 +0530

    Update index.js

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -3,46 +3,90 @@ import Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import './index.css'
 import VaccinationCoverage from '../VaccinationCoverage'
+import VaccinationByGender from '../VaccinationByGender'
+import VaccinationByAge from '../VaccinationByAge'
+
+const apiStatus = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN PROGRESS',
+}
 
 class CowinDashboard extends Component {
-  state = {isLoading: true, vaccinationData: {}}
+  state = {loadingStatus: apiStatus.initial, vaccinationData: {}}
 
   componentDidMount() {
     this.getVaccinationList()
   }
 
   getVaccinationList = async () => {
+    this.setState({loadingStatus: apiStatus.inProgress})
     const response = await fetch('https://apis.ccbp.in/covid-vaccination-data')
     if (response.ok === true) {
       const data = await response.json()
       const formattedData = {
         last7DaysVaccination: data.last_7_days_vaccination,
-        vaccinationByAge: data.vaccinationByAge,
-        vaccinationByGender: data.vaccinationByGender,
+        vaccinationByAge: data.vaccination_by_age,
+        vaccinationByGender: data.vaccination_by_gender,
       }
-      this.setState({isLoading: false, vaccinationData: formattedData})
+      this.setState({
+        loadingStatus: apiStatus.success,
+        vaccinationData: formattedData,
+      })
     } else {
-      this.setState({isLoading: false, vaccinationData: {}})
+      this.setState({loadingStatus: apiStatus.failure})
     }
   }
 
-  renderVaccinationDetails = () => {
-    const {vaccinationData} = this.state
-    if (vaccinationData.length !== 0) {
-      const {last7DaysVaccination} = vaccinationData
-      return <VaccinationCoverage coverageDetails={last7DaysVaccination} />
-    }
-    return (
+  renderLoaderPage = () => (
+    <div data-testid="loader" className="loader-card">
+      <Loader type="ThreeDots" color="#ffffff" height={80} width={80} />
+    </div>
+  )
+
+  renderFailurePage = () => (
+    <div className="failure-container">
       <img
         src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png "
         alt="failure view"
-        className="img-fail"
       />
+      <h1>Something Went Wrong</h1>
+    </div>
+  )
+
+  renderTheChartPage = () => {
+    const {vaccinationData} = this.state
+    const {
+      last7DaysVaccination,
+      vaccinationByAge,
+      vaccinationByGender,
+    } = vaccinationData
+
+    return (
+      <>
+        <VaccinationCoverage coverageDetails={last7DaysVaccination} />
+        <VaccinationByGender genderDetails={vaccinationByGender} />
+        <VaccinationByAge ageDetails={vaccinationByAge} />
+      </>
     )
   }
 
+  renderAllThePages = () => {
+    const {loadingStatus} = this.state
+    switch (loadingStatus) {
+      case apiStatus.success:
+        return this.renderTheChartPage()
+      case apiStatus.inProgress:
+        return this.renderLoaderPage()
+      case apiStatus.failure:
+        return this.renderFailurePage()
+      default:
+        return null
+    }
+  }
+
   render() {
-    const {isLoading} = this.state
     return (
       <div className="bg-container">
         <div className="vaccination-card-container">
@@ -53,14 +97,8 @@ class CowinDashboard extends Component {
             />
             <h1>co-WIN</h1>
           </div>
-          <p className="caption">co-WIN Vaccination in India</p>
-          {isLoading ? (
-            <div data-testid="loader" className="loader-card">
-              <Loader type="ThreeDots" color="#ffffff" height={80} width={80} />
-            </div>
-          ) : (
-            this.renderVaccinationDetails()
-          )}
+          <h1 className="caption">CoWIN Vaccination in India</h1>
+          {this.renderAllThePages()}
         </div>
       </div>
     )
